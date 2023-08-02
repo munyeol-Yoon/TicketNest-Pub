748118d32bfe85e0966e54a09bc36a5cb7a065d3
Shin Sungyun <sinsungyun1630@gmail.com>
2023-08-03 00:19:03 +0900

    Refactor : 회원가입 에러 응답 메시지 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,37 +19,57 @@ export class AuthService {
   constructor(
     @InjectRepository(UserEntity) // 레포지토리를 서비스에 주입
     private userRepository: Repository<UserEntity>,
-    private jwtService: JwtService,
-    private configService: ConfigService,
+    private jwtService: JwtService, // jwt 의존성 주입
+    private configService: ConfigService, // env 파일 읽게 하기 위함.
   ) {}
 
   async signUp(body: SignUpDto) {
     const { email, password, confirm, nickname } = body;
+    if (!body.email || !body.password || !body.confirm || !body.nickname) {
+      throw new BadRequestException({
+        errorMessage: '데이터 형식이 잘못되었습니다.',
+      });
+    }
+
     if (password !== confirm) {
       throw new BadRequestException(
         '확인 비밀번호가 입력 비밀번호와 일치하지 않습니다.',
       );
     }
     const isExistEmail = await this.userRepository.findOne({
-      where: { email: email },
+      where: [{ email: email }, { nickname: nickname }],
     });
-    if (isExistEmail) {
-      throw new NotFoundException('이미 존재하는 이메일 입니다');
-    }
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = this.userRepository.create({
-      email,
-      password: hashedPassword,
-      nickname,
-    });
-    this.userRepository.save(user);
-    return { message: '회원가입 성공' };
+    if (isExistEmail) {
+      if (isExistEmail.email === email) {
+        throw new NotFoundException('이미 존재하는 이메일 입니다');
+      }
+      if (isExistEmail.nickname === nickname) {
+        throw new NotFoundException('이미 존재하는 닉네임 입니다');
+      }
+    }
+
+    try {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const user = this.userRepository.create({
+        email,
+        password: hashedPassword,
+        nickname,
+      });
+      this.userRepository.save(user);
+      return { message: '회원가입 성공' };
+    } catch (err) {
+      throw new InternalServerErrorException({
+        errorMessage: '회원가입에 실패하였습니다',
+      });
+    }
   }
 
   async login(body: LoginDto) {
-    if (!body || !body.email || !body.password) {
+    // body.email, body.password 만 체크하면 될 거 같음.
+    if (!body.email || !body.password) {
       throw new BadRequestException({
         errorMessage: '데이터 형식이 잘못되었습니다.',
       });
