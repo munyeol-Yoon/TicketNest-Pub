8fbecd44d9b19e4509f0914b4c8a97bed94ec06d
Shin Sungyun <sinsungyun1630@gmail.com>
2023-08-19 09:54:29 +0900

    Fix : jwt 토큰 httponly true 설정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -31,7 +31,10 @@ export class AuthController {
   @Post('login')
   async login(@Body() body: LoginDto, @Res() res: Response) {
     const { accessToken } = await this.authService.login(body);
-    // res.cookie('Authorization', token);
+    res.cookie('Authorization', accessToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
     return res.json({ accessToken });
   }
 }
