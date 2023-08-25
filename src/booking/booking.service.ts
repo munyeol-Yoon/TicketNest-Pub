356fd0f94f534aa810403a29894030cbf3d879e3
hangju0610 <phj950610@naver.com>
2023-08-25 23:43:23 +0900

    Feat : redlock 분산락 구현

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -10,10 +10,12 @@ import { DataSource, Repository } from 'typeorm';
 import { GoodsEntity } from '../goods/entities/goods.entity';
 import * as apm from 'elastic-apm-node';
 import Redis from 'ioredis';
+import Redlock from 'redlock';
 
 @Injectable()
 export class BookingService {
   private redisClient: Redis;
+  private redlock: Redlock;
   constructor(
     @InjectRepository(BookingEntity)
     private bookingRepository: Repository<BookingEntity>,
@@ -23,55 +25,53 @@ export class BookingService {
     @Inject('REDIS_CLIENT') redisClient: Redis,
   ) {
     this.redisClient = redisClient;
+    this.redlock = new Redlock([redisClient as any], {
+      driftFactor: 0.01, // clock drift를 보상하기 위해 driftTime 지정에 사용되는 요소, 해당 값과 아래 ttl값을 곱하여 사용.
+      retryCount: 10, // 에러 전까지 재시도 최대 횟수
+      retryDelay: 200, // 각 시도간의 간격
+      retryJitter: 2000, // 재시도시 더해지는 되는 쵀대 시간(ms)
+    });
   }
   async createBooking(goodsId: number, userId: number) {
-    //Transaction 적용을 위한 queryRunner 사용
-    const queryRunner = this.dataSource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction('READ COMMITTED');
-    const qb = queryRunner.manager.createQueryBuilder();
+    const qb = this.dataSource.createQueryBuilder();
+    const lockResource = [`goodsId:${goodsId}:lock`]; // 락을 식별하는 고유 문자열
+    const lock = await this.redlock.acquire(lockResource, 20000); // 20초 뒤에 자동 잠금해제
+
     try {
       // 1. 예매수 및 Limit 확인
       // span 추가
       const findGoodsSpan = apm.startSpan('findGoodsSpan');
-      const cachedBookingCount = await this.redisClient.get(
-        `goodsId:${goodsId}`,
-      );
-      const cachedBookingLimit = await this.redisClient.get(
+      const bookingCount = await this.redisClient.get(`goodsId:${goodsId}`);
+      const bookingLimit = await this.redisClient.get(
         `bookingLimitOfGoodsId:${goodsId}`,
       );
+      // if (!cachedBookingCount || !cachedBookingLimit) {
+      //   const findGoods = await qb
+      //     .select([
+      //       'GoodsEntity.id',
+      //       'GoodsEntity.bookingLimit',
+      //       'GoodsEntity.bookingCount',
+      //     ])
+      //     .from(GoodsEntity, 'GoodsEntity')
+      //     .where('id=:id', { id: goodsId })
+      //     .getOne();
 
-      let bookingCount: number;
-      let bookingLimit: number;
-
-      if (!cachedBookingCount || !cachedBookingLimit) {
-        const findGoods = await qb
-          .select([
-            'GoodsEntity.id',
-            'GoodsEntity.bookingLimit',
-            'GoodsEntity.bookingCount',
-          ])
-          .from(GoodsEntity, 'GoodsEntity')
-          .where('id=:id', { id: goodsId })
-          .setLock('pessimistic_write')
-          .getOne();
-
-        bookingCount = findGoods.bookingCount;
-        bookingLimit = findGoods.bookingLimit;
-        await this.redisClient.set(
-          `bookingLimitOfGoodsId:${findGoods.id}`,
-          bookingLimit,
-        );
-      } else {
-        // 레디스에서 가져온 데이터 타입은 스트링이므로 숫자로 변환
-        bookingCount = +cachedBookingCount;
-        bookingLimit = +cachedBookingLimit;
-      }
+      //   bookingCount = findGoods.bookingCount;
+      //   bookingLimit = findGoods.bookingLimit;
+      //   await this.redisClient.set(
+      //     `bookingLimitOfGoodsId:${findGoods.id}`,
+      //     bookingLimit,
+      //   );
+      // } else {
+      //   // 레디스에서 가져온 데이터 타입은 스트링이므로 숫자로 변환
+      //   bookingCount = +cachedBookingCount;
+      //   bookingLimit = +cachedBookingLimit;
+      // }
 
       findGoodsSpan.end();
 
       // 2. 예매 limit보다 많을 경우, Error 처리 진행
-      if (bookingCount >= bookingLimit) {
+      if (Number(bookingCount) >= Number(bookingLimit)) {
         //! throw 에러 처리를 하면 부하 테스트 단계에서 에러가 나서 일단 주석처리
         // throw new ConflictException({
         //   errorMessage: '남은 좌석이 없습니다.',
@@ -80,43 +80,18 @@ export class BookingService {
         return { message: '예매가 초과되어 대기자 명단에 등록 되었습니다' };
       }
 
-      // goods update ms측정
-      const goodsUpdateSpan = apm.startSpan('goodsUpdateSpan');
-      // GoodsEntity의 BookingCount 업데이트
-      await qb
-        .update(GoodsEntity)
-        .set({
-          bookingCount: () => 'bookingCount + 1',
-        })
-        .where('id = :id', { id: goodsId })
-        .useTransaction(true)
-        .execute();
-
-      goodsUpdateSpan.end();
-
-      //Booking insert ms 측정
-      const bookingSaveSpan = apm.startSpan('bookingSaveSpan');
-      // bookingEntity Insert 진행
-      await qb
-        .insert()
-        .into(BookingEntity)
-        .values({
-          goodsId,
-          userId,
-        })
-        .useTransaction(true)
-        .execute();
-      await this.redisClient.incr(`goodsId:${goodsId}`); // 이 부분 분산 트랜잭션 필요함.
-
-      bookingSaveSpan.end();
-
-      await queryRunner.commitTransaction();
+      const pubSpan = apm.startSpan('pubSpan');
+      await this.redisClient.publish(
+        'Ticket',
+        JSON.stringify({ goodsId, userId }),
+      );
+      await this.redisClient.incr(`goodsId:${goodsId}`);
+      pubSpan.end();
     } catch (err) {
-      await queryRunner.rollbackTransaction();
       // error를 Transaction 블록 외부로 던지기 위함
       throw err;
     } finally {
-      await queryRunner.release();
+      lock.release();
     }
     // Transaction 정상 시 성공 메세지 출력
     return { message: '예약 성공!' };
